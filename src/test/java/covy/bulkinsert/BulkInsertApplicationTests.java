4e4b42fbb138f90548f5344c8ad8ddf91fce8a76
covy <junni802@naver.com>
2025-02-05 22:28:58 +0900

    first commit

---
+++ src/test/java/covy/bulkinsert/BulkInsertApplicationTests.java
@@ -0,0 +1,13 @@
+package covy.bulkinsert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkInsertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
